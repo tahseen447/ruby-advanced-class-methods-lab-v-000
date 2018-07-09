ab3c0345a83d8eca2be52089a285144d4b514072
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-09 07:45:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,10 +49,10 @@ class Song
   #  @@all.sort_by! {|song| song.name}
   end
 
-    def self.new_from_filename(song_filename)
+    def self.new_from_filename(filename)
     end
 
-    def self.create_from_filename
+    def self.create_from_filename(filename)
     end
 
     def self.destroy_all

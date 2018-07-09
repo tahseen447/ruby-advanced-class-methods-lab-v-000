4c3482036d879eef795b09d23cc2887885cf619e
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-09 07:11:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ class Song
 
   def self.find_or_create_by_name(name)
     new_song = self.find_by_name(name)
-    new_song = self.create_by_name(name)
+  #  new_song = self.create_by_name(name)
     new_song
   end
 end
